95eb8cfd156a76cee02ec879051e3656fda658d9
Boro23 <404slimboysavage@gmail.com>
2021-01-13 19:55:18 +0530

    initial-commit

---
+++ config/database.js
@@ -0,0 +1,16 @@
+// require("dotenv").config();
+
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "mongoose",
+      settings: {
+        uri: env("DATABASE_URI"),
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1dfc5881-5f35-4b8f-989f-8bd11bb6635a'
+};
